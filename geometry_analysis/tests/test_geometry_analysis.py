44703da6fc0041fe79c743ce5bf942ad63f27cb3
M.Z. Makos <M.Z.Makos@Pro-2.local>
2019-07-25 11:46:11 -0500

    add measure distances and angles

--- geometry_analysis/tests/test_geometry_analysis.py
+++ geometry_analysis/tests/test_geometry_analysis.py
@@ -7,6 +7,32 @@ import geometry_analysis
 import pytest
 import sys
 
+
+import numpy as np
+
 def test_geometry_analysis_imported():
     """Sample test, will always pass so long as import statement worked"""
-    assert "geometry_analysis" in sys.modules
+    assert "geometry_analysis" in sys.modules 
+
+def test_calculate_distance():
+
+	r1 = np.array([0, 0, -1])
+	r2 = np.array([0, 1, 0])
+
+	expected_distance = np.sqrt(2.)
+
+	calculated_distance = geometry_analysis.measure.calculate_distance(r1, r2)
+
+	assert expected_distance == calculated_distance
+
+def test_calculate_angle():
+
+	a1 = np.array([1, 0, 0])
+	a2 = np.array([0, 0, 0])
+	a3 = np.array([0, 1, 0])
+
+	expected_angle =90 
+
+	calculated_anlge = geometry_analysis.measure.calculate_angle(a1, a2, a3, degrees = True)
+	assert calculated_anlge == expected_angle
+
